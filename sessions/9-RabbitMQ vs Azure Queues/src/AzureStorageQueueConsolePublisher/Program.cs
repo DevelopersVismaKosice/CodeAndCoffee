e25736fb8e5a783d66ccfe77f774597eac2a5998
Arpad Restei <arpadres@youforce.net>
2020-10-22 15:27:09 +0200

    committing again

---
+++ sessions/9-RabbitMQ vs Azure Queues/src/AzureStorageQueueConsolePublisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+
+namespace AzureStorageQueueConsolePublisher
+{
+    public static class Program
+    {
+        private static readonly string ConnectionString = "Replace this text with the connection string to your own Azure Storage Queue";
+
+        private static readonly string StoregeQueueName = "demo-storage-queue";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Storage Queue Demo - Console Publisher");
+
+            QueueClient queue = new QueueClient(ConnectionString, StoregeQueueName);
+
+            Console.WriteLine("Enter a message:");
+            var message = Console.ReadLine();
+
+            await InsertMessageAsync(queue, message);
+            Console.WriteLine($"Sent: {message}");
+        }
+
+        static async Task InsertMessageAsync(QueueClient theQueue, string newMessage)
+        {
+            if (null != await theQueue.CreateIfNotExistsAsync())
+            {
+                Console.WriteLine("The queue was created.");
+            }
+
+            await theQueue.SendMessageAsync(newMessage);
+        }
+    }
+}
